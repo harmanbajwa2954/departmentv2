997283f5a444d830dc57c8a202eda4819e122bca
Harmanpreet Singh <harmanbajwa012005@gmail.com>
2025-04-20 12:58:26 +0530

    Minor UI improvements

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'auth/login_screen.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
